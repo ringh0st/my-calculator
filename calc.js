bcd529e265485e914283756d802ae06e54872156
Rinat Nadav <ringhost@gmail.com>
2020-09-01 17:29:48 -0300

    added onclick function

---
+++ calc.js
@@ -0,0 +1,14 @@
+
+
+function pushButton(buttonKey)
+{
+    console.log(buttonKey);
+    //  if (buttonKey === 'AC')
+    //  {
+    //       document.getElementById('screen').value = '';
+    //  }
+    //  else
+    //  {
+    //       document.getElementById('screen').value += buttonKey;
+    //  }
+}
