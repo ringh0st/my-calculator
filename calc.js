afbdcf9aa1f1dad24513840a53d1828cf5231a00
Rinat Nadav <ringhost@gmail.com>
2020-09-03 19:37:24 -0400

    outputScreen is updating its value

--- calc.js
+++ calc.js
@@ -1,14 +1,46 @@
+class Calculator{
+    constructor(outputScreen){
+        this.outputScreen = outputScreen
+        this.clear()
+        
+    }
+    clear(){
+        // this.outputScreen = '';
+        this.oparation = undefined;
+    }
+    delete(){
 
+    }
+    appendNumber(number){
+        console.log(this.outputScreen)
+        this.outputScreen.value = number
 
-function pushButton(buttonKey)
-{
-    console.log(buttonKey);
-    //  if (buttonKey === 'AC')
-    //  {
-    //       document.getElementById('screen').value = '';
-    //  }
-    //  else
-    //  {
-    //       document.getElementById('screen').value += buttonKey;
-    //  }
+    }
+    chooseOparation(oparation){
+
+    }
+    compute(){
+        
+    }
+    updateDisplay(){
+        console.log(this.outputScreen)
+        this.outputScreen.innerText = this.outputScreen
+
+    }
 }
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-oparation]');
+const equalButton = document.querySelector('[data-equal]');
+const deleteButton = document.querySelector('[data-delete]');
+const outputScreen = document.querySelector('[data-output]');
+
+const calculator = new Calculator(outputScreen);
+
+numberButtons.forEach(button =>{
+    button.addEventListener('click',()=>{
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+        // console.log(outputScreen.innerText = button.innerText)
+    })
+})
